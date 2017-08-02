b5ac28ac3ecd4b21d46bea9a73f69050c086cb42
eclancy <eric.clancy@maine.edu>
2017-08-02 13:48:24 -0400

    Plays a random insult
    #4 Solves number 4 with a sprinkle of a goat scream. DUMP THE WAVS ON IN!

--- Assets/Resources/Scripts/pushHandler.cs
+++ Assets/Resources/Scripts/pushHandler.cs
@@ -4,22 +4,33 @@ using UnityEngine;
 
 public class pushHandler : MonoBehaviour {
 
+    int i = 0;
 
+    public AudioSource[] insults;
 
+    public AudioClip[] testinsult;
 
-    // Use this for initialization
-    void Start () {
+// Use this for initialization
+void Start () {
+        Debug.Log("test");
+        testinsult = Resources.LoadAll<AudioClip>("Sounds/");
+        Debug.Log(testinsult[1]);
     }
 
     // Update is called once per frame
     void Update () {
+    }
 
-
-		
-	}
-    void OnMouseDown() { 
-        Debug.Log("test");
+    void OnMouseDown() {
         AudioSource audio = GetComponent<AudioSource>();
+
+        int clipvalue = (int)(Random.value * testinsult.Length);
+            print(clipvalue);
+            audio.clip = testinsult[clipvalue];
+            
+
+        Debug.Log(testinsult);
+
         audio.Play();
 
         GetComponent<SpriteRenderer>().sprite = Resources.Load("Buttons/buttonpushed", typeof(Sprite)) as Sprite;
